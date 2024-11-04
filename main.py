ec48eb94ba5b94ada5acbb217fed665b44ccfe18
= <andrewhrapkov@yandex.ru>
2024-11-04 15:11:06 +0300

    added swagger

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-# main.py
 from aiohttp import web
 import asyncio
-from aiohttp.web_request import Request
 from db import init_db
-# Импортируем модель, чтобы она регистрировалась в метаданных базы данных
 from routes import task_routes
-from models import Task
+from aiohttp_swagger3 import SwaggerDocs, SwaggerUiSettings
 
 app = web.Application()
 app.add_routes(task_routes)
 
+# Инициализация Swagger документации
+swagger = SwaggerDocs(
+    app,
+    title="Task API",
+    version="1.0.0",
+    description="API для управления задачами",
+    swagger_ui_settings=SwaggerUiSettings(path="/docs")  # путь к Swagger UI
+)
+swagger.add_routes(task_routes)
+
+
+async def startup_db_init(app):
+    await init_db()
+
+app.on_startup.append(startup_db_init)
+
 if __name__ == '__main__':
-    asyncio.run(init_db())
     web.run_app(app, port=9000)
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-# routest.py
+import datetime
 from aiohttp import web
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -10,8 +10,47 @@ task_routes = web.RouteTableDef()
 
 @task_routes.get('/task/{id}')
 async def get_task_by_id(request):
+    """
+    ---
+    description: Получить задачу по ID
+    tags:
+      - Tasks
+    parameters:
+      - in: path
+        name: id
+        description: ID задачи
+        required: true
+        schema:
+          type: integer
+    responses:
+      "200":
+        description: Успешное получение задачи
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                id:
+                  type: integer
+                text:
+                  type: string
+                status:
+                  type: string
+                created_at:
+                  type: string
+                updated_at:
+                  type: string
+      "404":
+        description: Задача не найдена
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                error:
+                  type: string
+    """
     task_id = request.match_info['id']
-
     async with async_session() as session:
         async with session.begin():
             result = await session.execute(select(Task).where(Task.id == int(task_id)))
@@ -29,3 +68,69 @@ async def get_task_by_id(request):
         }
 
         return web.json_response(task_data)
+
+
+@task_routes.post('/task')
+async def create_task(request):
+    """
+    ---
+    description: Создать новую задачу
+    tags:
+      - Tasks
+    requestBody:
+      required: true
+      content:
+        application/json:
+          schema:
+            type: object
+            properties:
+              text:
+                type: string
+                description: Текст задачи
+              status:
+                type: boolean
+                description: Статус задачи (например, "в работе" или "завершена")
+            required:
+              - text
+              - status
+    responses:
+      "201":
+        description: Задача успешно создана
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                id:
+                  type: integer
+                text:
+                  type: string
+                status:
+                  type: string
+                created_at:
+                  type: string
+                updated_at:
+                  type: string
+    """
+    data = await request.json()
+
+    async with async_session() as session:
+        task_data = {
+            "text": data.get('text'),
+            "status": data.get('status'),
+            "created_at": datetime.datetime.utcnow()
+        }
+
+        async with session.begin():
+            new_task = Task(
+                text=task_data["text"],
+                status=task_data["status"],
+                created_at=task_data["created_at"],
+                updated_at=None
+            )
+            session.add(new_task)
+
+        await session.commit()
+        await session.refresh(new_task)
+        task_data['created_at'] = task_data['created_at'].__str__()
+        return web.json_response(task_data, status=201)
